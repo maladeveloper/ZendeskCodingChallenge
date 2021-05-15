738e6ce44972bfd9e8ca35daf0e78db001192d4a
msri0004@student.monash.edu <msri0004@student.monash.edu>
2021-05-15 11:47:32 +1000

    Finished testing frontend

--- frontend/src/__test__/Component.test.js
+++ frontend/src/__test__/Component.test.js
@@ -9,16 +9,34 @@ configure({ adapter: new Adapter() });
 import HomePage from "../Scripts/HomePage";
 import TicketViewer from "../Scripts/TicketViewer";
 import TicketPane from '../Scripts/TicketPane';
+import SingleTicket from '../Scripts/SingleTicket';
+
+//Vars 
+var  mockedTicketData = require("./MockedTicketData.json")
 
 
-describe("Testing if non-dependant components render without crashing", () =>{
 
-  it("HomePage renders without crashing", () => {
+describe("Testing if independant components render without crashing", () =>{
+
+  it("HomePage renders without crashing.", () => {
     shallow(<HomePage />);
   });
 
-  it("TicketViewer rendeders without crashing", ()=>{
+  it("TicketViewer rendeders without crashing.", ()=>{
     shallow(<TicketViewer/>)
   })
 
+})
+
+describe("Testing if components with props input render without crashing.", () =>{
+  it("TicketPane renders without crashing", () =>{
+
+    shallow(<TicketPane tickets={mockedTicketData.tickets}/>)
+  })
+
+  it("SingleTicket renders without crashing.", ()=>{
+
+    shallow(<SingleTicket ticket={mockedTicketData.tickets[0]}/>)
+  })
+  
 })
